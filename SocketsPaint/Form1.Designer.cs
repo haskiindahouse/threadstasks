3a75c1c762c271aac0cf4c1f663109d8ed41c778
haskiindahouse <haskiindahouse@gmail.com>
2022-12-16 11:03:02 +0300

    continue work on sockets graphic chat

--- SocketsPaint/Form1.Designer.cs
+++ SocketsPaint/Form1.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(485, 436);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(508, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Socket";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- SocketsPaint/Form1.cs
+++ SocketsPaint/Form1.cs
@@ -4,9 +4,13 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace SocketsPaint
 {
@@ -15,6 +19,56 @@ namespace SocketsPaint
         public Form1()
         {
             InitializeComponent();
+        }
+
+        bool _isDrawing = false;
+        Point _startPosition = Point.Empty;
+        Bitmap bmp = new Bitmap(500, 500);
+        bool isReserving = false;
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _isDrawing = true;
+            _startPosition = e.Location;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!_isDrawing)
+                return;
+
+            _startPosition = e.Location;
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.FillEllipse(Brushes.Red, new Rectangle(_startPosition.X, _startPosition.Y, 4, 4));
+            }
+
+            panel1.Invalidate();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(bmp, new Point(0, 0));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            int port = 5000;
+            TcpClient client = new TcpClient();
+            client.Connect(ip, port);
+            Console.WriteLine("client connected!!");
+            NetworkStream ns = client.GetStream();
+            Thread thread = new Thread(o => ReceiveData((TcpClient)o, ref bmp));
+            thread.Start(client);
+        }
+
+
+        static void ReceiveData(TcpClient client, ref Bitmap bmp)
+        {
+            NetworkStream ns = client.GetStream();
+            BinaryFormatter bin = new BinaryFormatter();
+            bmp = (Bitmap)bin.Deserialize(ns);
         }
     }
 }
